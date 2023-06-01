5c1d2194c6f9988b2e27d811ec0eef1af31c28a6
mai refaat <mairefaat2019@gmail.com>
2023-06-01 17:44:48 +0300

    review edit done

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,45 +6,75 @@ const app = express.Router();
 
 // POST a new review
 app.post("/", async (req, res) => {
-    const { user, productId, title, comment, rating } = req.body;
-    try {
-      // Check if user exists
-      const existingUser = await User.findOne({ nationalId: user });
-      if (!existingUser) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      // Create new review
-      const review = new Review({
-        user: existingUser.nationalId,
-        product: productId,
-        title,
-        comment,
-        rating,
-      });
-      const savedReview = await review.save();
-      res.status(200).json({
-                status: "success",
-                message: "Your Review added successfully",
-                savedReview,
-              });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Server Error" });
+  const { user, productId, title, comment, rating } = req.body;
+  try {
+    // Check if user exists
+    const existingUser = await User.findOne({ nationalId: user });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
 
+    // Create new review
+    const review = new Review({
+      user: existingUser.nationalId,
+      product: productId,
+      title,
+      comment,
+      rating,
+    });
+    const savedReview = await review.save();
+
+    savedReview.profileImage = existingUser.profileImage;
+    savedReview.userName = existingUser.name;
+
+    res.status(200).json({
+      status: "success",
+      message: "Your review was added successfully",
+      savedReview: {
+        user: savedReview.user,
+        profileImage: savedReview.profileImage,
+        userName: savedReview.userName,
+        product: savedReview.product,
+        title: savedReview.title,
+        comment: savedReview.comment,
+        rating: savedReview.rating,
+        _id: savedReview._id,
+        createdAt: savedReview.createdAt,
+        updatedAt: savedReview.updatedAt,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
 // Get all reviews for a specific product
 app.get("/allreviews/:productId", async (req, res) => {
   try {
     const reviews = await Review.find({ product: req.params.productId })
-      .populate("user", "name") // Populate user information with name field
       .sort("-createdAt"); // Sort reviews in descending order by creation date
+
+    const reviewsWithUserInfo = await Promise.all(reviews.map(async (review) => {
+      const user = await User.findOne({ nationalId: review.user });
+      return {
+        _id: review._id,
+        user: {
+          name: user.name,
+          profileImage: user.profileImage,
+        },
+        product: review.product,
+        title: review.title,
+        comment: review.comment,
+        rating: review.rating,
+        createdAt: review.createdAt,
+      };
+    }));
 
     res.status(200).json({
       status: "success",
       message: "All reviews for a specific product",
-      reviews,
+      reviews: reviewsWithUserInfo,
     });
   } catch (error) {
     console.error(error);
