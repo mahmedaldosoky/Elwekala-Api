f091b77725494e292dc563bc419026452ab515a5
Mohamed Aldosoky <37597841+mahmedaldosoky@users.noreply.github.com>
2023-06-07 17:54:33 +0300

    fix password > 8

--- mongodb/models/user.js
+++ mongodb/models/user.js
@@ -32,10 +32,10 @@ const userSchema = mongoose.Schema({
       message: (props) => `${props.value} is not a valid national ID!`,
     },
   },
-  profileImage: { 
-    type: String, 
-    required: true
-   },
+  profileImage: {
+    type: String,
+    required: true,
+  },
   gender: {
     type: String,
     enum: ["male", "female"],
